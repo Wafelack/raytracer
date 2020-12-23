7a8261ef9a89299aa6852a9d3523cae4c335172f
Wafelack <wafelack@protonmail.com>
2020-12-23 20:48:35 +0100

    feat: added blurred perlin noise

--- src/perlin.rs
+++ src/perlin.rs
@@ -29,11 +29,42 @@ impl Perlin {
     }
   }
   pub fn noise(&self, p: &point3) -> f32 {
-    let i = (4. * p.x()) as i32 & 255;
-    let j = (4. * p.y()) as i32 & 255;
-    let k = (4. * p.z()) as i32 & 255;
-    self.ranfloat
-      [(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize]
+    let u = p.x() - p.x().floor();
+    let v = p.y() - p.y().floor();
+    let w = p.z() - p.z().floor();
+
+    let i = p.x().floor() as i32;
+    let j = p.y().floor() as i32;
+    let k = p.z().floor() as i32;
+
+    let mut c = [[[0.; 2]; 2]; 2];
+
+    for di in 0..2 {
+      for dj in 0..2 {
+        for dk in 0..2 {
+          c[di][dj][dk] = self.ranfloat[(self.perm_x[(i + di as i32) as usize & 255]
+            ^ self.perm_y[(j + dj as i32) as usize & 255]
+            ^ self.perm_z[(k + dk as i32) as usize & 255])
+            as usize];
+        }
+      }
+    }
+
+    Self::trilinear_interp(c, u, v, w)
+  }
+  fn trilinear_interp(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    let mut accum = 0.;
+    for i in 0..2 {
+      for j in 0..2 {
+        for k in 0..2 {
+          accum += (i as f32 * u as f32 + (1. - i as f32) * (1. - u))
+            * (j as f32 * v as f32 + (1. - j as f32) * (1. - v))
+            * (k as f32 * w as f32 + (1. - k as f32) * (1. - w))
+            * c[i][j][k];
+        }
+      }
+    }
+    accum
   }
   fn generate_perm(point_count: usize) -> Vec<i32> {
     let mut p = Vec::with_capacity(point_count);
