cb05d3f400e1826407b57bc388e12d7270f406ee
Wafelack <wafelack@protonmail.com>
2020-12-23 17:10:17 +0100

    fix: fixing sorting bug (but now it works because i'm smart)

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,4 +1,5 @@
 use crate::{aabb::*, objects::hittable::*, objects::hittable_list::*, ray::*, utils::*};
+use std::ops::Deref;
 use std::sync::Arc;
 
 pub struct BvhNode {
@@ -8,21 +9,22 @@ pub struct BvhNode {
 }
 
 impl BvhNode {
-  pub fn from(list: &HittableList, time0: f32, time1: f32) -> Self {
-    Self::fromvec(&list.objects, 0, list.objects.len(), time0, time1)
+  pub fn from(list: &mut HittableList, time0: f32, time1: f32) -> Self {
+    let len = &mut list.objects.len();
+    Self::fromvec(&mut list.objects, 0, *len, time0, time1)
   }
   pub fn fromvec(
-    src_objects: &Vec<Arc<dyn Hittable>>,
+    src_objects: &mut Vec<Arc<dyn Hittable>>,
     start: usize,
     end: usize,
     time0: f32,
     time1: f32,
   ) -> Self {
-    let mut objects = src_objects;
+    let objects = src_objects;
     let axis = random_int(0, 2);
 
-    let mut left = objects[start];
-    let mut right = objects[start];
+    let mut left: Arc<dyn Hittable> = objects[start].clone();
+    let mut right: Arc<dyn Hittable> = objects[start].clone();
 
     let comparator = if axis == 0 {
       box_x_compare
@@ -35,14 +37,40 @@ impl BvhNode {
 
     if object_span == 1 {
     } else if object_span == 1 {
-      if comparator(objects[start], objects[start + 1]) {
-        left = objects[start];
-        right = objects[start + 1];
+      if comparator(objects[start].deref(), objects[start + 1].deref()) {
+        left = objects[start].clone();
+        right = objects[start + 1].clone();
       } else {
-        right = objects[start];
-        left = objects[start + 1];
+        right = objects[start].clone();
+        left = objects[start + 1].clone();
       }
     } else {
+      &objects[start..end].sort_unstable_by(|a, b| {
+        if comparator(&**a, &**b) {
+          std::cmp::Ordering::Greater
+        } else {
+          std::cmp::Ordering::Less
+        }
+      });
+
+      let mid = start + object_span / 2;
+      left = Arc::new(BvhNode::fromvec(objects, start, mid, time0, time1)) as Arc<dyn Hittable>;
+    }
+    let mut box_left = Aabb::new();
+    let mut box_right = Aabb::new();
+
+    if !left.bounding_box(time0, time1, &mut box_left)
+      || !right.bounding_box(time0, time1, &mut box_right)
+    {
+      eprintln!("No bounding box in bvh_node constructor");
+    }
+
+    let boxx = surrounding_box(&box_left, &box_right);
+
+    Self {
+      left: left.clone(),
+      right: right.clone(),
+      boxx,
     }
   }
 }
@@ -73,4 +101,24 @@ impl Hittable for BvhNode {
 
     hit_left || hit_right
   }
+}
+
+fn box_compare(a: &dyn Hittable, b: &dyn Hittable, axis: usize) -> bool {
+  let mut box_a = Aabb::new();
+  let mut box_b = Aabb::new();
+
+  if !a.bounding_box(0., 0., &mut box_a) || !b.bounding_box(0., 0., &mut box_b) {
+    eprintln!("No bounding box in BvhNode constructor");
+  }
+  box_a.min().e[axis] < box_b.min().e[axis]
+}
+
+fn box_x_compare(a: &dyn Hittable, b: &dyn Hittable) -> bool {
+  box_compare(a, b, 0)
+}
+fn box_y_compare(a: &dyn Hittable, b: &dyn Hittable) -> bool {
+  box_compare(a, b, 1)
+}
+fn box_z_compare(a: &dyn Hittable, b: &dyn Hittable) -> bool {
+  box_compare(a, b, 2)
 }
